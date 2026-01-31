5ad385bed02e51b706efed53e42fdb7ddfe90f5c
Erkhembileg Ariunbold <a.erkhembileg9@gmail.com>
2026-01-30 21:53:12 -0700

    Learning react: creating root, createElement, calling render, and making a custom react component.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createElement } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootEl = document.getElementById("root");
+if (!rootEl) {
+  throw new Error("Root element not found!");
+}
+// Create Root and Render it
+const root = createRoot(rootEl);
+// createElement returns a javascript object
+// <h1> <span>...</span> </h1>
+const reactElement = createElement(
+  "h1",
+  null,
+  createElement("span", null, "I'm inside the span."),
+);
+const plainhtmlElement = (
+  <h1>
+    <span>Here is the plain Html element!</span>
+  </h1>
+);
+
+// Very first custom React component!
+function MainContent() {
+  return <h1> React is great!</h1>;
+}
+
+// render takes a single argument
+root.render(
+  // element, props, children
+  //reactElement,
+  //plainhtmlElement,
+  <div>
+    <MainContent />  {/*This is how we call custom component*/}
+  </div>,
+);
